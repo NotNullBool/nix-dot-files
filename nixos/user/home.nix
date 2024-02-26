01b0d56304a0473bb2b09155a517cd0a280b3b77
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2024-02-25 19:31:01 -0500

    fzf theme and addition

--- nixos/user/home.nix
+++ nixos/user/home.nix
@@ -68,6 +68,23 @@
             '';
         };
 
+        fzf = {
+            enable = true;
+            colors = {
+                fg="-1";
+                bg="-1";
+                hl = "#5fff87";
+                "fg+" = "-1";
+                "bg+" = "-1";
+                "hl+" = "#ffaf5f";
+                info = "#af87ff";
+                prompt = "#5fff87";
+                pointer = "#ff87d7";
+                marker = "#ff87d7";
+                spinner = "#ff87d7";
+            };
+        };
+
         lf = { enable = true; };
 
         zsh = {
