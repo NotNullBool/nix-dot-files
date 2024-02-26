9f6611af134d51e2da5a706ae2ad0c354428f6f0
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2024-02-25 19:49:30 -0500

    enable nvim

--- nixos/user/home.nix
+++ nixos/user/home.nix
@@ -30,6 +30,14 @@
 
 
     programs = {
+        neovim = {
+            enable = true;
+            defaultEditor = true;
+            viAlias = true;
+            vimAlias = true;
+            withNodeJs = true;
+            withPython3 = true;
+        };
 
         home-manager = {
             enable = true;
