7f03f8295a3b9f04ad39435a9a2d558520aa3233
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2024-02-28 21:10:37 -0500

    replaced bufferline with cokeline for transparency

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,9 +1,29 @@
 return {
 	'akinsho/bufferline.nvim',
+	enabled=false,
 	version = "*",
 	dependencies = 'nvim-tree/nvim-web-devicons',
-	opts = function ()
+	config = function ()
 		vim.opt.termguicolors = true
-		return {}
+		require("bufferline").setup()
+        vim.cmd([[
+
+            autocmd ColorScheme * highlight BufferLineFill guibg=none
+
+            highlight BufferLineFillSelected guibg=none
+
+            highlight BufferLineFillVisible guibg=none
+
+            highlight BufferLineDevIconLua guibg=none
+
+            highlight BufferLineCloseButton guibg=none
+
+            highlight BufferLineBackground guibg=none
+
+            highlight BufferLineSeparator guifg=none
+
+            highlight BufferLineSeparatorSelected guifg=none
+
+        ]])
 	end,
 }
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -111,8 +111,8 @@ return{{
 			}
 		})
 
-		vim.cmd[["highlight Pmenu guibg=NONE"]] -- transparent background for autocompletion
-
+		vim.cmd("highlight Pmenu guibg=NONE" ) -- transparent background for autocompletion
+        
 	end
 },{ "pixldev/bevy-snippets", event = "InsertEnter *.rs", config = function ()
 		require("luasnip.loaders.from_vscode").lazy_load()
