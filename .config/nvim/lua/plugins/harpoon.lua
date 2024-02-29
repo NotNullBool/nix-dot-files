bb5b943d2304f3de38b5294234ff6dfdf6b46904
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2024-02-29 12:18:26 -0500

    harpoon mark fix

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -2,7 +2,7 @@ return {
     "ThePrimeagen/harpoon",
     keys = {
         {"<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Toggle Harpoon Menu"},
-        {"<leader>ha", "<cmd>lua require('harpoon.ui').add_file()<CR>", desc = "Harpoon Add File"},
+        {"<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>", desc = "Harpoon Add File"},
     },
     config = true,
 }
