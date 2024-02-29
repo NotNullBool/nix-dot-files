229afc5aff4df2e9ed689294ef63c394500e31ec
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2024-02-29 12:02:56 -0500

    cokeline fg brighter

--- .config/nvim/lua/plugins/nvim-cokeline.lua
+++ .config/nvim/lua/plugins/nvim-cokeline.lua
@@ -5,5 +5,16 @@ return  {
         "nvim-tree/nvim-web-devicons", -- If you want devicons
         "stevearc/resession.nvim"       -- Optional, for persistent history
     },
-    opts = true
+    opts = {
+
+        default_hl = {
+            ---@type nil | string | fun(buffer: Buffer): string
+            fg = function(buffer)
+                local hlgroups = require("cokeline.hlgroups")
+                return buffer.is_focused and hlgroups.get_hl_attr("Normal", "fg")
+                    or hlgroups.get_hl_attr("ColorColumn", "bg")
+            end,
+        }
+
+    }
 }
