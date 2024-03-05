33b00df0b0a8e8b5531035b1a9b705e64df067b5
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2024-03-04 19:21:08 -0500

    tree sitter replaced with internal method

--- nixos/user/programs/nvim.nix
+++ nixos/user/programs/nvim.nix
@@ -1,4 +1,33 @@
 { pkgs, ... }:
+let
+    treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+        p.bash
+        p.comment
+        p.css
+        p.dockerfile
+        p.fish
+        p.gitattributes
+        p.gitignore
+        p.go
+        p.gomod
+        p.gowork
+        p.hcl
+        p.javascript
+        p.jq
+        p.json5
+        p.json
+        p.lua
+        p.make
+        p.markdown
+        p.nix
+        p.python
+        p.rust
+        p.toml
+        p.typescript
+        p.vue
+        p.yaml
+    ]));
+in
 {
     home.packages = with pkgs; [
         fd
@@ -16,6 +45,15 @@
         vimAlias = true;
         withNodeJs = true;
         withPython3 = true;
+
+        plugins = [
+            treesitterWithGrammars
+        ];
+    };
+
+    home.file."./.local/share/nvim/nix/nvim-treesitter/" = {
+        recursive = true;
+        source = treesitterWithGrammars;
     };
 
     # programs.opam.enable = true; # ocaml stuff
