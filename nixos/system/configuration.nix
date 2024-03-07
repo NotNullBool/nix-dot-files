acbc31e049e85725ded6fcf3c921db4a891e5605
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2024-03-07 17:22:20 -0500

    nix: nvidia drivers setup

--- nixos/system/configuration.nix
+++ nixos/system/configuration.nix
@@ -16,6 +16,21 @@
         ./wm/qtile/qtile.nix
     ];
 
+    services.xserver.videoDrivers = ["nvidia"];
+
+    hardware.nvidia = {
+        modesetting.enable = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        open = false;
+        nvidiaSettings = true;
+        prime = {
+            sync.enable = true;
+
+            intelBusId = "PCI:0:2:0";
+            nvidiaBusId = "PCI:1:0:0";
+        };
+    };
+
     qt.enable = true;
 
     hardware.keyboard.qmk.enable = true;
